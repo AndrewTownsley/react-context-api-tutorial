e549f2eb6288a4d0521eca304a26d28c1ac27cc2
Andrew TOwnsley <atownsley86@gmail.com>
2022-04-13 21:01:27 -0600

    Add to cart function works from producrt detail page!.

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { CartState } from '../../Context/Context';
 import { Link, useLocation } from 'react-router-dom';
 import { Button } from '../../StyleProps';
-import Rating from '../../components/Rating';
+import RatingOnCard from '../../components/RatingOnCard';
 import { AiOutlineShoppingCart, AiOutlineClose } from 'react-icons/ai';
 import { ProductDetailWrapper, DetailContent, DetailContentImg, DetailContentInfo, DetailCartBtnCont } from './ProductDetailStyle'
 // import { ProductCardBtnCont } from '../../components/ProductItem/ProductItemStyle';
@@ -14,6 +14,7 @@ const ProductDetail = ({ product }) => {
   } = CartState();
   const location = useLocation();
   const [inStock, setInStock] = useState(location.state.product.inStock)
+  console.log(cart);
 
   return (
     <ProductDetailWrapper>
@@ -30,18 +31,17 @@ const ProductDetail = ({ product }) => {
             <h1>{location.state.product.name}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus officiis dignissimos eaque tempore, recusandae commodi corrupti consectetur aliquid molestiae? Eius necessitatibus quisquam quos quidem cupiditate quo repellendus voluptate accusamus officiis nostrum similique, nulla eum ipsum amet, aperiam ea.</p>
             <h2>${location.state.product.price}</h2>
-            <Rating rating={location.state.product.rating} />
+            <RatingOnCard rating={location.state.product.rating}/>
               <DetailCartBtnCont>
 
-                      { cart.some(p => p.id === location.state.product.id ) ?
+                      { cart.some(p => p.name === location.state.product.name ) ?
                         (
                           <Button
-                          // className={inStock !== 0 ? "" : "out-of-stock"}
                           onClick={() => {
                             setInStock(inStock + 1)
                             dispatch({  
                               type: "REMOVE_FROM_CART",
-                              payload: product
+                              payload: location.state.product
                             })
                           }}
                           >
@@ -60,7 +60,7 @@ const ProductDetail = ({ product }) => {
                               setInStock(inStock -1)
                               dispatch({
                                 type: "ADD_TO_CART",
-                                payload: product
+                                payload: location.state.product
                               })
                             }}
                             >
