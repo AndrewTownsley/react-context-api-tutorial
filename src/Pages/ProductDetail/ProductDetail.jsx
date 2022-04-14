03aa9107fc185fe671eb10f6c9facc5ba4432d8d
Andrew TOwnsley <atownsley86@gmail.com>
2022-04-13 20:47:03 -0600

    Add to cart function works from producrt detail page!.

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CartState } from '../../Context/Context';
 import { Link, useLocation } from 'react-router-dom';
 import { Button } from '../../StyleProps';
 import Rating from '../../components/Rating';
-import { ProductDetailWrapper, DetailContent, DetailContentImg, DetailContentInfo } from './ProductDetailStyle'
+import { AiOutlineShoppingCart, AiOutlineClose } from 'react-icons/ai';
+import { ProductDetailWrapper, DetailContent, DetailContentImg, DetailContentInfo, DetailCartBtnCont } from './ProductDetailStyle'
+// import { ProductCardBtnCont } from '../../components/ProductItem/ProductItemStyle';
 
-const ProductDetail = ({ product, state }) => {
-    const location = useLocation();
-    console.log(location)
+const ProductDetail = ({ product }) => {
+  const { 
+    state: { cart },
+    dispatch 
+  } = CartState();
+  const location = useLocation();
+  const [inStock, setInStock] = useState(location.state.product.inStock)
 
   return (
     <ProductDetailWrapper>
@@ -24,9 +31,50 @@ const ProductDetail = ({ product, state }) => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus officiis dignissimos eaque tempore, recusandae commodi corrupti consectetur aliquid molestiae? Eius necessitatibus quisquam quos quidem cupiditate quo repellendus voluptate accusamus officiis nostrum similique, nulla eum ipsum amet, aperiam ea.</p>
             <h2>${location.state.product.price}</h2>
             <Rating rating={location.state.product.rating} />
-            <Button>
-              Add to Cart
-            </Button>
+              <DetailCartBtnCont>
+
+                      { cart.some(p => p.id === location.state.product.id ) ?
+                        (
+                          <Button
+                          // className={inStock !== 0 ? "" : "out-of-stock"}
+                          onClick={() => {
+                            setInStock(inStock + 1)
+                            dispatch({  
+                              type: "REMOVE_FROM_CART",
+                              payload: product
+                            })
+                          }}
+                          >
+                             <AiOutlineClose 
+                                      style={{fontSize: '1rem', paddingRight: '5px'}} 
+                             />
+                            Remove
+                          </Button>
+                        )
+                        : 
+                        (
+                          <>
+                          <Button 
+                            className={inStock !== 0 ? "" : "out-of-stock"} 
+                            onClick={() => {
+                              setInStock(inStock -1)
+                              dispatch({
+                                type: "ADD_TO_CART",
+                                payload: product
+                              })
+                            }}
+                            >
+                              <AiOutlineShoppingCart 
+                                      style={{fontSize: '1rem', paddingRight: '5px'}} 
+                                  />
+                            Add to Cart
+                          </Button>
+                        </>
+
+                        ) 
+                      }
+                </DetailCartBtnCont>
+          
           </DetailContentInfo>
         </DetailContent>
     </ProductDetailWrapper>
--- src/Pages/ProductDetail/ProductDetailStyle.js
+++ src/Pages/ProductDetail/ProductDetailStyle.js
@@ -43,3 +43,8 @@ export const DetailContentInfo = styled.div`
         margin: 0.5rem 0 0 0;
     }
 `
+
+export const DetailCartBtnCont = styled.div`
+    display: flex;
+    
+`
